6369ea1f4bd7c6dc8f3f35b75f76463981300b28
Saransh Kumar <saransh.kumar0707@gmail.com>
2024-07-17 02:02:40 +0530

    updating weather page

--- server.py
+++ server.py
@@ -23,8 +23,17 @@ def get_weather_route():
     return flask.render_template(
         "weather.html",
         title = weather_data['name'],
-        decription = weather_data['weather'][0]['main'],
-        temp = weather_data['main']['temp']
+        decription = weather_data['weather'][0]['description'],
+        temp = weather_data['main']['temp'],
+        feel = weather_data['main']['feels_like'],
+        mintemp = weather_data['main']['temp_min'],
+        maxtemp = weather_data['main']['temp_max'],
+        press = weather_data['main']['pressure'],
+        humid = weather_data['main']['humidity'],
+        visi = weather_data['visibility'],
+        wspeed = weather_data['wind']['speed'],
+        wdir = weather_data['wind']['deg'],
+        icon = f"http://openweathermap.org/img/wn/{weather_data['weather'][0]['icon']}@2x.png"
     )
 
 if __name__ == '__main__':
